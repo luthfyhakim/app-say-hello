2d442e22c2db6a7b832ecf6aba2f2be663cb5db5
luthfyhakim <luthfyhakim250404@gmail.com>
2022-12-31 06:45:27 +0700

    update versi major

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/luthfyhakim/app-say-hello
 
 go 1.19
 
-require github.com/luthfyhakim/go-say-hello v1.1.0
+require github.com/luthfyhakim/go-say-hello/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/luthfyhakim/go-say-hello"
+	// update major menggunakan versi tags
+	go_say_hello "github.com/luthfyhakim/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Hakim"))
 }
